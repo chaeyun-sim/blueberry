433e16c433240467d5f9da1f1e45d5a064c9b50d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-03-02 23:12:03 +0900

    fix(Index): 의미없는 이미지에 alt 내용 제거

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -176,7 +176,7 @@ const Dashboard = () => {
           <img
             src='https://images.unsplash.com/photo-1771506364945-0b6566c6cd5f?q=80&w=2232&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
             className='w-full h-full object-fill lg:object-center'
-            alt="Dashboard Background"
+            alt=""
           />
         </div>
         {/* Top Row */}
