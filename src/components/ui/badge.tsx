222b9e9353c68ef6a036c67ac09883599279fe62
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-03-04 15:19:41 +0900

    fix(badge): 중복 렌더링 수정

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -5,8 +5,8 @@ import { badgeVariants } from './badge.style';
 
 export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> {}
 
-function Badge({ className, variant, ...props }: BadgeProps) {
-  return <div className={cn(badgeVariants({ variant }), className)} {...props}>{props.children}</div>;
+function Badge({ className, variant, children, ...props }: BadgeProps) {
+  return <div className={cn(badgeVariants({ variant }), className)} {...props}>{children}</div>;
 }
 
 export default Badge;
